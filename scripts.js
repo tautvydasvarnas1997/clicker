1c3fdff91d77de2c5ccdabb6b5cd3369f6cabb70
tautvydasvarnas1997 <tautvydas.varnas.97@gmail.com>
2024-12-12 15:07:24 +0200

    Added functionality to buttons

--- scripts.js
+++ scripts.js
@@ -9,8 +9,23 @@ window.addEventListener("load", function() {
 
     this.document.querySelectorAll('.factory').forEach(function(fc) {
         let factory = {
-            title: fc.querySelector('.title').innerText
+            title: fc.querySelector('.title').innerText,
+            costs: parseInt(fc.querySelector('.price').innerText),
+            isAdding: parseInt(fc.querySelector('.adds').innerText),
+            count: parseInt(fc.querySelector('.count').innerText.slice(1,-1)),
+            button: fc.querySelector('button')
         };
-        console.log(factory);
+        factory.button.onclick = function() {
+            if (score >= factory.costs) {
+                score -= factory.costs;
+                factory.count++;
+                scoreBlock.innerText = score;
+                fc.querySelector('.count').innerText = '(' + factory.count + ')';
+        }
+    }
+        ownedFactories.push(factory);
     });
+        console.log(factory);
 });
+console.log(ownedFactories);
+
